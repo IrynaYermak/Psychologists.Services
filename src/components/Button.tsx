783e1813c46856a75fd6827e96c5610c55798fb3
Iryna Yermak <1991irenka@gmail.com>
2026-06-09 18:13:49 +0100

    initial commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,44 @@
+import css from "./Button.module.css";
+import clsx from "clsx";
+import { Link } from "react-router-dom";
+
+interface BaseButtonProps {
+  children: React.ReactNode;
+  variant?: "primary" | "secondary";
+  size: "medium" | "large";
+  text: string;
+  "aria-label"?: string;
+}
+
+interface ButtonElementProps extends BaseButtonProps {
+  type?: "button" | "submit";
+  onClick?: () => void;
+  to?: never;
+}
+
+interface LinkElementProps extends BaseButtonProps {
+  to: string;
+  type?: never;
+  onClick?: never;
+}
+
+type ButtonProps = ButtonElementProps | LinkElementProps;
+
+export default function Button(props: ButtonProps) {
+  const { variant = "primary", size = "medium", children } = props;
+  const className = clsx(css.button, css[variant], css[size]);
+
+  if ("to" in props) {
+    return (
+      <Link to={props.to} className={className}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button type={props.type} className={className} onClick={props.onClick}>
+      {children}
+    </button>
+  );
+}
